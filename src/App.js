3e33a18b863aeefe523052d8caceea1b0a06daef
Enot Hueda <enot.hueda@gmail.com>
2020-03-30 21:42:05 -0600

    added the redux saga utility and was added the sagas functions to users signin with email and google, signout and signup, also for cart and clear cart and the shop as an async method

--- src/App.js
+++ src/App.js
@@ -11,29 +11,32 @@ import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up
 import CheckoutPage from './pages/checkout/checkout.component';
 import ContactPage from './pages/contact/contact.component';
 import Header from './components/header/header.component';
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
-import { setCurrentUser } from './redux/user/user.actions';
+
 import { selectCurrentUser } from './redux/user/user.selector';
+import { checkUserSession } from './redux/user/user.actions';
 
 class App extends React.Component { 
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    const { setCurrentUser } = this.props
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-      if (userAuth) {
-        const userRef = await createUserProfileDocument(userAuth);
+    const { checkUserSession } = this.props;
+    checkUserSession();
 
-        userRef.onSnapshot(snapShot => {
-          setCurrentUser({
-            id: snapShot.id,
-            ...snapShot.data()
-          });          
-        });        
-      } else {
-        setCurrentUser(userAuth);
-      }
-    });  
+    // this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+    //   if (userAuth) {
+    //     const userRef = await createUserProfileDocument(userAuth);
+
+    //     userRef.onSnapshot(snapShot => {
+    //       setCurrentUser({
+    //         id: snapShot.id,
+    //         ...snapShot.data()
+    //       });          
+    //     });        
+    //   } else {
+    //     setCurrentUser(userAuth);
+    //   }
+    // });  
+    
   }
 
   componentWillUnmount() {
@@ -66,10 +69,10 @@ class App extends React.Component {
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser
-})
+});
 
 const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
+  checkUserSession: () => dispatch(checkUserSession())
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,52 +2,32 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-import { firestore, convertCollectionSnapshotToMap } from '../../firebase/firebase.utils';
+import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
 
-import { updateCollections } from '../../redux/shop/shop.actions';
+import CollectionPageContainer from '../collection/collection.container';
+import CollectionOverviewContainer from '../../components/collection-overview/collection-overview.container';
 
-import CollectionPage from '../collection/collection.component';
-import CollectionOverview from '../../components/collection-overview/collection-overview.component';
-import WithSpinner from '../../components/with-spinner/with-spinner.component';
-
-const CollectionOverviewWithSpinner = WithSpinner(CollectionOverview);
-const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
 class ShopPage extends React.Component{
-    state = {
-        loading: true
-    };
-
-    unsubscribeFromSnapshot = null;
 
     componentDidMount() {
-        const { updateCollections } = this.props;
-        const collectionRef = firestore.collection('collections');
-
-        collectionRef.get().then(snapshot => {
-            const collectionMap = convertCollectionSnapshotToMap(snapshot);
-            updateCollections(collectionMap);
-            this.setState({ loading: false })
-        })        
+        const { fetchCollectionsStart } = this.props;
+        fetchCollectionsStart();
     }
 
     render() {
         const { match } = this.props;
-        const { loading } = this.state;
+        
         return (
             <div className='shop-page'>
                 <Route 
                     exact 
                     path={`${match.path}`}
-                    render={(props) => 
-                        <CollectionOverviewWithSpinner isLoading={loading} {...props} />
-                    }
+                    component={CollectionOverviewContainer}
                 />
                 <Route 
                     path={`${match.path}/:collectionId`}
-                    render={(props) => 
-                        <CollectionPageWithSpinner isLoading={loading} {...props} /> 
-                    }
+                    component={CollectionPageContainer}
                 />
             </div>
         )
@@ -55,8 +35,9 @@ class ShopPage extends React.Component{
 
 }
 
+
 const mapDispatchToProps = dispatch => ({
-    updateCollections: collectionMap => dispatch(updateCollections(collectionMap))
+    fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
 })
 
 export default connect(null, mapDispatchToProps)(ShopPage);
