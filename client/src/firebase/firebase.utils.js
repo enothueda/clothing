1031ed6d8cada53e63e33c3ae2a4ee3657c2f30b
Enot Hueda <enot.hueda@gmail.com>
2020-04-14 19:38:20 -0500

    upgrading some security vulnerabilities advised from github

--- client/src/firebase/firebase.utils.js
+++ client/src/firebase/firebase.utils.js
@@ -2,16 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-const config = {
-    apiKey: "AIzaSyAwKhOQAbrgIWLZSPHOGbH8lpJOPmQxUw0",
-    authDomain: "clothing-db-dcfdc.firebaseapp.com",
-    databaseURL: "https://clothing-db-dcfdc.firebaseio.com",
-    projectId: "clothing-db-dcfdc",
-    storageBucket: "clothing-db-dcfdc.appspot.com",
-    messagingSenderId: "820020769697",
-    appId: "1:820020769697:web:3cc1d8c41c1777e5b011e0",
-    measurementId: "G-GWBH5F48PR"
-};
+const config = 'YOUR API KEY AND INFORMATION HERE'
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
     if(!userAuth) return;
